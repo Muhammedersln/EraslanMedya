303a9755fdb3963d148057b5ef71da006c92a642
Muhammed Eraslan <muhammed.ersln@icloud.com>
2024-12-24 20:54:32 +0300

    test mod kapa

--- src/app/api/payment/paytr/route.js
+++ src/app/api/payment/paytr/route.js
@@ -29,7 +29,7 @@ export async function POST(req) {
     const merchant_fail_url = 'https://eraslanmedya.com/payment/fail';
     const timeout_limit = '30';
     const debug_on = '1';
-    const test_mode = "1";
+    const test_mode = "0";
     const no_installment = '0';
     const max_installment = '0';
     const currency = 'TL';
